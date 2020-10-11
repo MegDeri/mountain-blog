52c8161d3e58aa30de907f5f517a110cb0583683
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-10-11 21:02:03 +0300

    Add routers & controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+//bring routers
+const blogRoutes = require('./routes/blog')
+
 //app
 const app = express();
 
@@ -19,15 +22,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//routes middleware
+app.use('/api', blogRoutes);
+
 //cors
 if (process.env.NODE_ENV === "development") {
     app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
-
-//routes
-app.get('/api', (req, res) => {
-    res.json({ time: Date().toString() });
-});
 
 //port
 const port = process.env.PORT || 8000
